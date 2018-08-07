02bd25f8342f6ad23737ef3f63bec11a55a081e8
Omar Cruz <ocruzvar@gmail.com>
2018-08-06 20:46:04 -0500

    added motion

--- src/networks.js
+++ src/networks.js
@@ -23,5 +23,15 @@ module.exports = {
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef
+  },
+  motion: {
+    messagePrefix: '\x19Motion Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x32,
+    scriptHash: 0x12,
+    wif: 0x80
   }
 }
